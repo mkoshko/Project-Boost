7fabd8aeead09d265bda322a05c34a2ca3cb98f4
maksim_kashko <maksim_kashko@epam.com>
2021-01-10 18:01:31 +0300

    add audio source

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -6,26 +6,42 @@ public class Rocket : MonoBehaviour
 {
 
     Rigidbody rigidbody;
+    AudioSource audioSource;
     // Start is called before the first frame update
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
+
+    }
+
+    private void FixedUpdate()
+    {
         if (Input.GetKey(KeyCode.Space))
         {
             rigidbody.AddRelativeForce(Vector3.up);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+                audioSource.loop = true;
+            }
+        }
+        else
+        {
+            audioSource.Stop();
         }
         if (Input.GetKey(KeyCode.A))
         {
-
+            transform.Rotate(-Vector3.forward);
         }
         if (Input.GetKey(KeyCode.D))
         {
-
+            transform.Rotate(Vector3.forward);
         }
     }
 }
